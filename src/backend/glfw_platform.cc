3452051d43225c3c42df892f5ba4d1906463a56e
Teamkiller131 <ws@tkiller.com>
2026-05-25 19:42:53 +0800

    fix: use gladLoadGLLoader(glfwGetProcAddress) for AMD GL loading
    - gladLoadGL() may not correctly resolve function pointers on AMD
    - gladLoadGLLoader() with glfwGetProcAddress is the recommended way
    - Also revert to GL 3.3 CORE + GLSL 330 core
    - Fix GLAD pointer format string (%p -> {})
    - 185/185 tests pass

--- src/backend/glfw_platform.cc
+++ src/backend/glfw_platform.cc
@@ -23,8 +23,8 @@ public:
         UNIGUI_LOG_DEBUG("glfwInit OK");
 
         glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         window_ = glfwCreateWindow(1280, 720, "UniGUI", nullptr, nullptr);
